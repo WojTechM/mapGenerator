445567eb51759d646e070b7bea0a2d6edf1e0cfe
WojTechM <wojciech.m1997@gmail.com>
2021-01-24 10:00:18 +0100

    init: implement Field class

--- src/main/java/com/github/wojtechm/Field.java
+++ src/main/java/com/github/wojtechm/Field.java
@@ -4,5 +4,33 @@ package com.github.wojtechm;
  * @author Wojciech Makieła
  */
 public class Field {
+    private int metersAboveSeaLevel;
+    private TerrainType terrainType;
+
+    public Field(int metersAboveSeaLevel, TerrainType terrainType) {
+        this.metersAboveSeaLevel = metersAboveSeaLevel;
+        this.terrainType = terrainType;
+    }
+
+    public Field(int metersAboveSeaLevel) {
+        this.metersAboveSeaLevel = metersAboveSeaLevel;
+        this.terrainType = TerrainType.GRASSLAND;
+    }
+
+    public int getMetersAboveSeaLevel() {
+        return metersAboveSeaLevel;
+    }
+
+    public void setMetersAboveSeaLevel(int metersAboveSeaLevel) {
+        this.metersAboveSeaLevel = metersAboveSeaLevel;
+    }
+
+    public TerrainType getTerrainType() {
+        return terrainType;
+    }
+
+    public void setTerrainType(TerrainType terrainType) {
+        this.terrainType = terrainType;
+    }
 
 }
