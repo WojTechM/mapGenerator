3731ea3263006c662c4fbd051346efec76b7734b
MateuszGolda <mateusz.golda1993@gmail.com>
2021-01-24 14:51:05 +0200

    nitpick: change board size

--- src/main/java/com/github/wojtechm/Main.java
+++ src/main/java/com/github/wojtechm/Main.java
@@ -1,5 +1,6 @@
 package com.github.wojtechm;
 
+import com.github.wojtechm.display.DisplayFactory;
 import com.github.wojtechm.landformgenerator.LandformGenerator;
 import com.github.wojtechm.terraingenerator.TerrainGenerator;
 import com.github.wojtechm.watergenerator.WaterGenerator;
@@ -10,9 +11,13 @@ import com.github.wojtechm.watergenerator.WaterGenerator;
 class Main {
 
     public static void main(String[] args) {
-        GeneratedMap map = new GeneratedMap(); // TODO: take dimensions from args
+        GeneratedMap map = new GeneratedMap(90, 40); // TODO: take dimensions from args
+//        GeneratedMap map = new GeneratedMap(450, 165);
+
         LandformGenerator lg = new LandformGenerator();
         lg.generate(map);
+        DisplayFactory.physicalDisplay().print(map);
+
         WaterGenerator wg = new WaterGenerator();
         wg.generate(map);
         TerrainGenerator tg = new TerrainGenerator();
