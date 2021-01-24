41d4644c487ffdb10bf3d84827c63ade730b25c8
WojTechM <wojciech.m1997@gmail.com>
2021-01-24 11:04:43 +0100

    refactor: reformatted code in entire project

--- src/main/java/com/github/wojtechm/GeneratedMap.java
+++ src/main/java/com/github/wojtechm/GeneratedMap.java
@@ -1,7 +1,6 @@
 package com.github.wojtechm;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
--- src/main/java/com/github/wojtechm/Point.java
+++ src/main/java/com/github/wojtechm/Point.java
@@ -14,15 +14,24 @@ public class Point {
 
     public Point moveInDirection(Direction direction) {
         switch (direction) {
-            case NORTH: return new Point(X, Y + 1);
-            case NORTHEAST: return new Point(X + 1, Y + 1);
-            case EAST: return new Point(X + 1, Y);
-            case SOUTHEAST: return new Point(X + 1, Y - 1);
-            case SOUTH: return new Point(X, Y - 1);
-            case SOUTHWEST: return new Point(X - 1, Y - 1);
-            case WEST: return new Point(X - 1, Y);
-            case NORTHWEST: return new Point(X - 1, Y + 1);
-            default: throw new IllegalArgumentException("Unsupported Direction");
+            case NORTH:
+                return new Point(X, Y + 1);
+            case NORTHEAST:
+                return new Point(X + 1, Y + 1);
+            case EAST:
+                return new Point(X + 1, Y);
+            case SOUTHEAST:
+                return new Point(X + 1, Y - 1);
+            case SOUTH:
+                return new Point(X, Y - 1);
+            case SOUTHWEST:
+                return new Point(X - 1, Y - 1);
+            case WEST:
+                return new Point(X - 1, Y);
+            case NORTHWEST:
+                return new Point(X - 1, Y + 1);
+            default:
+                throw new IllegalArgumentException("Unsupported Direction");
         }
     }
 }
